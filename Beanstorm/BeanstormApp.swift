a7f2c583753b5438a9f6370ef769b25de75a6b81
LeonPS <leon@leonps.com>
2023-12-17 16:59:47 +0000

    Initial Commit

---
+++ Beanstorm/BeanstormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeanstormApp.swift
+//  Beanstorm
+//
+//  Created by Leon Paterson-Stephens on 17/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct BeanstormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
