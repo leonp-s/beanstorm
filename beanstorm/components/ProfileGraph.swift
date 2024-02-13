8909688d161e64e85f94adf6d52c5fc3c4452a53
LeonPS <leon@leonps.com>
2024-02-13 22:53:13 +0000

    Profile graph improvements

--- beanstorm/components/ProfileGraph.swift
+++ beanstorm/components/ProfileGraph.swift
@@ -1,14 +1,14 @@
 import SwiftUI
 import Charts
 
-struct ChartPosition: Identifiable {
+struct ControlPoint: Identifiable {
     let id: UUID
-    let x: Double
-    let y: Double
+    let time: Double
+    let value: Double
 }
 
 struct ProfileGraph: View {
-    @State var positions: [ChartPosition]
+    @State var positions: [ControlPoint]
     @State var cursorIndex: Int?
     
     @State var posX: Double = 0.0
@@ -20,101 +20,181 @@ struct ProfileGraph: View {
         return colorScheme == .dark ? .white : .black
     }
     
+    func selectControlPoint(at: CGPoint, geometry: GeometryProxy, proxy: ChartProxy) {
+        let origin = geometry[proxy.plotFrame!].origin
+        let pos = proxy.value(atX: at.x - origin.x, as: Double.self)!
+        let closest = positions
+                        .enumerated()
+                        .min( by: { abs($0.element.time - pos) < abs($1.element.time - pos) } )!
+        
+
+        let index = closest.offset
+        if cursorIndex == index {
+            cursorIndex = nil
+        } else {
+            let pos = positions[index]
+            posX = pos.time
+            posY = pos.value
+            cursorIndex = index
+        }
+    }
+    
+    func updateCursor(at: CGPoint, geometry: GeometryProxy, proxy: ChartProxy) {
+        let origin = geometry[proxy.plotFrame!].origin
+
+        let time = proxy.value(atX: at.x - origin.x, as: Double.self)!
+        let value = proxy.value(atY: at.y - origin.y, as: Double.self)!
+
+        if let index = cursorIndex {
+            let pos = positions[index]
+            positions[index] = ControlPoint(id: pos.id, time: time, value: value)
+            posX = time
+            posY = value
+        }
+    }
+    
+    private var controlPointEditor: some View {
+        Group {
+            if cursorIndex != nil {
+                Form {
+                    Section(header: Text("Time")) {
+                        VStack {
+                            Slider(
+                                value: $posX,
+                                in: 0...10
+                            ) {
+                                Text("Values from 0 to 10")
+                            } minimumValueLabel: {
+                                Text("0")
+                            } maximumValueLabel: {
+                                Text("10")
+                            }
+                            .onChange(of: posX) {
+                                if let index = cursorIndex {
+                                    let pos = positions[index]
+                                    positions[index] = ControlPoint(id: pos.id, time: posX, value: pos.value)
+                                }
+                            }
+                            
+                            Text(String(format: "%.1f", posX))
+                                .font(.headline)
+                        }
+                    }
+                    
+                    Section(header: Text("Value")) {
+                        VStack {
+                            Slider(
+                                value: $posY,
+                                in: 0...1
+                            ) {
+                                Text("Values from 0 to 1")
+                            } minimumValueLabel: {
+                                Text("0")
+                            } maximumValueLabel: {
+                                Text("1")
+                            }
+                            .onChange(of: posY) {
+                                if let index = cursorIndex {
+                                    let pos = positions[index]
+                                    positions[index] = ControlPoint(id: pos.id, time: pos.time, value: posY)
+                                }
+                            }
+                            
+                            Text(String(format: "%.1f", posY))
+                                .font(.headline)
+                        }
+                    }
+                }
+            }
+        }
+        .animation(.spring, value: cursorIndex)
+    }
+    
+    var profileGraph: some View {
+        Chart {
+            ForEach(positions) { pos in
+                AreaMark(
+                    x: .value("Time", pos.time),
+                    y: .value("Height", pos.value)
+                )
+                .interpolationMethod(.catmullRom)
+                .foregroundStyle(
+                            .linearGradient(
+                                colors: [.blue, .cyan, .purple],
+                                startPoint: .bottom, endPoint: .top
+                            )
+                            .opacity(0.4)
+                        )
+                .alignsMarkStylesWithPlotArea()
+
+                LineMark(
+                    x: .value("Time", pos.time),
+                    y: .value("Height", pos.value)
+                )
+                .interpolationMethod(.catmullRom)
+                .foregroundStyle(
+                    .linearGradient(
+                        colors: [.blue, .cyan, .purple],
+                        startPoint: .bottom, endPoint: .top
+                    )
+                )
+                .lineStyle(StrokeStyle(lineWidth: 4))
+                .alignsMarkStylesWithPlotArea()
+            }
+            if let index = cursorIndex {
+                let position = positions[index]
+                RuleMark(x: .value("Time", position.time))
+                    .foregroundStyle(cursorColor)
+                RuleMark(y: .value("Height", position.value))
+                    .foregroundStyle(cursorColor)
+                PointMark(x: .value("Time", position.time), y: .value("Height", position.value))
+                    .foregroundStyle(cursorColor)
+                    .symbol(BasicChartSymbolShape.circle.strokeBorder(lineWidth: 3.0))
+                    .symbolSize(250)
+            }
+        }
+        .chartOverlay { proxy in
+            GeometryReader { geometry in
+                Rectangle().fill(.clear).contentShape(Rectangle())
+                    .gesture(
+                        DragGesture().onChanged { value in
+                            if(cursorIndex != nil) {
+                                updateCursor(
+                                    at: value.location,
+                                    geometry: geometry,
+                                    proxy: proxy
+                                )
+                            }
+                        }
+                    )
+                    .onTapGesture { location in
+                        selectControlPoint(
+                            at: location,
+                            geometry: geometry,
+                            proxy: proxy
+                        )
+                    }
+            }
+        }
+    }
+    
     var body: some View {
         VStack {
-            Chart {
-                ForEach(positions) { pos in
-                    AreaMark(
-                        x: .value("Time", pos.x),
-                        y: .value("Height", pos.y)
-                    )
-                }
-                if let index = cursorIndex {
-                    let position = positions[index]
-                    RuleMark(x: .value("Time", position.x))
-                        .foregroundStyle(cursorColor)
-                    RuleMark(y: .value("Height", position.y))
-                        .foregroundStyle(cursorColor)
-                    PointMark(x: .value("Time", position.x), y: .value("Height", position.y))
-                        .foregroundStyle(cursorColor)
-                        .symbol(BasicChartSymbolShape.circle.strokeBorder(lineWidth: 3.0))
-                        .symbolSize(250)
-                }
-            }
-            .chartOverlay { proxy in
-                GeometryReader { geometry in
-                    Rectangle().fill(.clear).contentShape(Rectangle())
-                        .gesture(DragGesture().onChanged { value in updateCursorPosition(at: value.location, geometry: geometry, proxy: proxy) })
-                        .onTapGesture { location in updateCursorPosition(at: location, geometry: geometry, proxy: proxy) }
-                }
-            }
-                
-            Group {
-                if cursorIndex != nil {
-                    
-                    Slider(
-                        value: $posX,
-                        in: 0...10
-                    ) {
-                        Text("Speed")
-                    } minimumValueLabel: {
-                        Text("0")
-                    } maximumValueLabel: {
-                        Text("10")
-                    }
-                    .onChange(of: posX) {
-                        if let index = cursorIndex {
-                            var poss = positions
-                            let pos = poss[index]
-                            positions[index] = ChartPosition(id: pos.id, x: posX, y: pos.y)
-                        }
-                    }
-                    Slider(
-                        value: $posY,
-                        in: 0...1
-                    ) {
-                        Text("Speed")
-                    } minimumValueLabel: {
-                        Text("0")
-                    } maximumValueLabel: {
-                        Text("1")
-                    }
-                    .onChange(of: posY) {
-                        if let index = cursorIndex {
-                            var poss = positions
-                            let pos = poss[index]
-                            positions[index] = ChartPosition(id: pos.id, x: pos.x, y: posY)
-                        }
-                    }
-                }
-            }
-            .animation(.spring, value: cursorIndex)
+            profileGraph
+            controlPointEditor
         }
-        .padding()
     }
-       
-      func updateCursorPosition(at: CGPoint, geometry: GeometryProxy, proxy: ChartProxy) {
-          let data = positions
-          let origin = geometry[proxy.plotAreaFrame].origin
-          let pos = proxy.value(atX: at.x - origin.x, as: Double.self)
-          let firstGreater = data.lastIndex(where: { $0.x < pos! })
-          
-          if let index = firstGreater {
-              if cursorIndex == index {
-                  cursorIndex = nil
-              } else {
-                  let pos = positions[index]
-                  posX = pos.x
-                  posY = pos.y
-                  cursorIndex = index
-              }
-          }
-      }
 }
 
 #Preview {
-    ProfileGraph(positions: [ChartPosition(id: UUID(), x: 0.0, y: 1.0),
-                    ChartPosition(id: UUID(), x: 4.0, y: 0.8),
-                    ChartPosition(id: UUID(), x: 6.0, y: 0.3),
-                    ChartPosition(id: UUID(), x: 10.0, y: 0.9)])
+    ProfileGraph(
+        positions: [
+            ControlPoint(id: UUID(), time: 0.0, value: 1.0),
+            ControlPoint(id: UUID(), time: 4.0, value: 0.8),
+            ControlPoint(id: UUID(), time: 6.0, value: 0.3),
+            ControlPoint(id: UUID(), time: 10.0, value: 0.9)
+        ]
+    )
+    .padding()
 }
+
